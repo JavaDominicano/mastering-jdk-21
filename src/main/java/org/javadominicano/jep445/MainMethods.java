749f7ccda04f236f0b5646f578bf4c333935d23f
brayanmnz <brayanmnz@hotmail.com>
2023-10-18 00:36:31 -0400

    jep-445 : Adding the class with the examples

---
+++ src/main/java/org/javadominicano/jep445/MainMethods.java
@@ -0,0 +1,45 @@
+
+/**
+ * This class MainMethods was written for
+ * the purpose of this demo. 
+ *
+ * In this class you'll find some differents
+ * main methods commented, please uncomment
+ * the main method that you want to run. 
+ *
+ * This is for just using one class for 
+ * the demonstration of how you can write 
+ * main methods now after JEP 445. 
+ *
+ *
+ */
+public class MainMethods {
+
+
+//Just uncomment the constructor for the example no. 2 - but, first run the example no. 1 also with the constuctor uncommented. 
+	
+	
+	/*public MainMethods(String args){
+		// just a constructor with argument for demonstration.
+	}*/
+	
+
+	// -- no. 1 without static access modifier and without String[] args
+	
+	void main(){
+		System.out.println("main method with no-static and no args");
+	}
+	
+
+	 // -- no. 2 with static access modifier and without String[] args
+	/*void main(){
+		System.out.println("main method with static modifier because the class contains a constructor with arguments");
+	}*/
+	
+
+	 // -- no. 3 with protected
+	/*protected void main(){
+		System.out.println("main method with protected, no-static and no-args");
+	}*/
+	
+}
